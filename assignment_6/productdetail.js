13b1ef2bcf0fda392b8645e647b29d9a3c5f25a0
ViviGong <gongweiuibe@gmail.com>
2018-10-26 17:00:48 -0400

    commit

--- assignment_6/productdetail.js
+++ assignment_6/productdetail.js
@@ -45,30 +45,68 @@ $(document).ready(function(){
     })
 })
 
+var cat_harness_1 = {
+    color: "Strawberry",
+    size: "Tiny",
+    Quantity: 0
+}
+
+
+function cat_harness_1(color, size, quantity) {
+    this.color = color,
+    this.size = size,
+    this.quantity = quantity
+}
 
 
 $(document).ready(function(){
     var cart_current = parseInt(JSON.parse(localStorage.getItem("database")));
     if (cart_current > 0) {
         $("#feedback").text(cart_current);
+        $('#selectedQty').text(cart_current);
+        $('#total_price').text((cart_current)*10)
     } else {
         $("#feedback").text("0");
+        $('#selectedQty').text("0");
+        $('#total_price').text('0');
     }
+
     var cart_total = parseInt($("#feedback").text());
     console.log(cart_total);
+
     $('#addtocart').click(function(){
         var itemCount = parseInt($('#qty').val());
         if (itemCount =="") {
             $("#feedback").innerHTML = "";
-        } else {
+        }
+        else {
             cart_total = cart_total + itemCount;
             localStorage.setItem("database", JSON.stringify(cart_total));
+
             console.log(
                 localStorage.getItem('database'));
             $('#feedback').text(cart_total);
+
+            // $('#selectedQty').text(cart_total)
+            // var color = $(this).siblings(".color");
+            // var size = $(this).siblings(".size");
+            // var quantity = $(this).quantity(".quantity");
+
+
+
+
         }
     })
 })
+
+
+
+
+
+
+
+
+
 
 
 
